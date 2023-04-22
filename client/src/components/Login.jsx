ba48f59a479900480e294c4364eb8c0519cc6c46
Magdalena Wild <maggieprophete2425@gmail.com>
2023-04-22 11:27:52 -0400

    adding login image

---
+++ client/src/components/Login.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import Bookshop from'../assets/Bookshop.png'
+
+export default function Login() {
+  return (
+    <div className='grid grid-cols-1 sm:grid-cols-2 h-screen w-full'>
+<div className='hiddden sm:block'>
+  <img className='w-full h-full object-cover' src={Bookshop} alt="" />
