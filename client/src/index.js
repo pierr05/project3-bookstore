aafb039c2073623591aacb60f93867e4dbec3e97
Magdalena Wild <maggieprophete2425@gmail.com>
2023-04-25 21:52:15 -0400

    sidebar and cart components

--- client/src/index.js
+++ client/src/index.js
@@ -4,12 +4,19 @@ import './index.css';
 import App from './App';
 
 import ProductProvider from './contexts/ProductContext';
+import SidebarProvider from './contexts/SidebarContext';
+import CartProvider from './contexts/CartContext';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById
+  ('root'));
 root.render(
-  <ProductProvider>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </ProductProvider>
+  <SidebarProvider>
+    <CartProvider>
+      <ProductProvider>
+        <React.StrictMode>
+          <App />
+        </React.StrictMode>
+      </ProductProvider>
+    </CartProvider>
+  </SidebarProvider>
 );
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -12,9 +12,8 @@ const Home = () => {
       "women books"
     );
   });
-  eturn (
+  return (
     <div>
-      <Hero />
       <section className='py-16'>
         <div className='container mx-auto'>
           <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 gap-[30px] max-w-sm mx-auto md:max-w-none md:mx-0'>
