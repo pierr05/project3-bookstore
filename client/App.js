7b92cb6a9fd38a1b4360a820e0d6a2360be32e6f
noah <noah.noah9372@gmail.com>
2023-04-28 16:25:24 -0400

    uploading back up

---
+++ client/App.js
@@ -0,0 +1,15 @@
+import Login from "./components/Login";
+// import carousel from "./components/carousel";
+
+
+function App() {
+  return(
+    <div>
+<Login/>
+    </div>
+  );
+}
+
+
+
+export default App;
