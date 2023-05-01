04c6575e9edb63abd85dd69152fb0d85ff7e027f
Magdalena Wild <maggieprophete2425@gmail.com>
2023-05-01 16:18:55 -0400

    fixing navbar error

--- client/src/pages/ProductPage.jsx
+++ client/src/pages/ProductPage.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import Product from '../components/Product'
-import NavBar from '../components/NavBar'
 
 function ProductPage() {
     return (
         <div>
-            <NavBar />
+            
             <Product />
         </div>
     );
