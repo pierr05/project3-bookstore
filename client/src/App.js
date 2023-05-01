2f28527d40612996f3da5f03ce90b57258a0c8d5
Magdalena Wild <maggieprophete2425@gmail.com>
2023-05-01 14:58:20 -0400

    adding product description and color change

--- client/src/App.js
+++ client/src/App.js
@@ -14,7 +14,7 @@ function App() {
       <Router>
         <Routes>
           <Route path='/' element={<Home/>} />
-          <Route path='/product/:id' element={<ProductPage/>} />
+          <Route path='/product/' element={<ProductPage/>} />
           <Route path='/login/' element={<LoginPage/>} />
           <Route path='/signup' element={<CreateAccountPage/>} />
           <Route path='/cart' element={<Cart/>} />
--- client/src/components/Product.jsx
+++ client/src/components/Product.jsx
@@ -24,7 +24,9 @@ const Product = () => {
                     <span className='font-semibold text-white'>headtitle</span>
                     <h1 className='text-3xl font-bold text-white'>Harry Potter Book</h1>
                 </div>
-                <p className='text-gray-200'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent rhoncus aliquam dui, aliquet faucibus odio mattis nec. Fusce ultricies tincidunt nisl at molestie. Donec vel elementum risus, at bibendum nunc. Pellentesque luctus purus sed urna sollicitudin, id sodales ex congue. Duis viverra posuere mi. Curabitur at lacinia libero. Nam sed porttitor lacus. Sed ultrices neque id quam finibus consectetur. Cras ut imperdiet metus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Aenean quam justo, auctor et nulla quis, cursus pellentesque arcu. Nam pharetra nisl quis luctus faucibus.
+                <p className='text-gray-200'>Harry Potter is a series of seven fantasy novels written by British author J. K. Rowling. The novels chronicle the lives of a young wizard, Harry Potter, and his friends Hermione Granger and Ron Weasley, all of whom are students at Hogwarts School of Witchcraft and Wizardry. The main story arc concerns Harry's conflict with Lord Voldemort, a dark wizard who intends to become immortal, overthrow the wizard governing body known as the Ministry of Magic and subjugate all wizards and Muggles (non-magical people).
+
+The series was originally published in English by Bloomsbury in the United Kingdom and Scholastic Press in the United States. All versions around the world are printed by Grafica Veneta in Italy. A series of many genres, including fantasy, drama, coming-of-age fiction, and the British school story (which includes elements of mystery, thriller, adventure, horror, and romance), the world of Harry Potter explores numerous themes and includes many cultural meanings and reference
                 </p>
                 <h6 className=' text-2xl font-semibold text-white'>$ 25.99</h6>
                 <div className='flex flex-row items-center gap-12'>
